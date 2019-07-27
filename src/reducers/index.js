705957128e6a53d6f3e9136abf6ac5b1b917c969
Aria Kim <shoo7830@gmail.com>
2019-07-28 01:10:31 +0900

    redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,26 +1,67 @@
 import * as types from '../actions/ActionTypes';
 
 const intialState = {
-    color: 'black',
-    number: 0
+    counters: [
+        {
+            color: 'black',
+            number: 0
+        }
+    ]
+    
 };
 
 function counter(state = intialState, action) {
+    const { counters } = state; 
+    
     switch (action.type) {
+        case types.CREATE:
+            return {
+                coutners: [
+                    ...counters,
+                    {
+                        color: action.color,
+                        number:0
+                    }
+                ]
+            };
+
+        case types.REMOVE: 
+            return {
+                counters: counters.slice(0, counters.length - 1)
+            };
+
         case types.INCREMENT:
             return {
-                ...state,
-                number: state.number + 1
-            }
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number + 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
+            };
         case types.DECREMENT:
             return {
-                ...state,
-                number: state.number - 1
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        number: counters[action.index].number - 1
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
             };
         case types.SET_COLOR:
             return {
-                ...state,
-                color: action.color
+                counters: [
+                    ...counters.slice(0, action.index),
+                    {
+                        ...counters[action.index],
+                        color: action.color
+                    },
+                    ...counters.slice(action.index + 1, counters.length)
+                ]
             };
         default:
             return state;
